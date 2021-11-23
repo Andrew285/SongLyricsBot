0d0187ddc339a0f62b57cd2d4d6b1ff574b3d048
Andriy_Kuzmych <75886738+Andrew285@users.noreply.github.com>
2021-11-23 10:46:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 song_bot = telebot.TeleBot("2128007635:AAHuDH8KQlA_RwNDSHE_v2ME2I4tHizdWV8")
 
 replaced_singer = ""
-replaced_song = ""
+# replaced_song = ""
 
 @song_bot.message_handler(content_types=["text"])
 def get_singer(message):
@@ -20,20 +20,21 @@ def get_song(message):
     global replaced_singer
     mssg = message.text
     replaced_song = mssg.replace(" ", "+")
+    song_bot.send_message(message.chat.id, f"{replaced_singer}+{replaced_song}")
 
     page = requests.get(f"https://www.google.com/search?q={replaced_singer}+{replaced_song}+lyrics")
     soup = BeautifulSoup(page.text, "lxml")
 
     str_text = soup.text
-    index_find = str_text.index("/")
+    start_index = str_text.index("/")
     last_index = str_text.index("Текст пісні")
 
-    result_text = str_text[index_find: last_index]
+    result_text = str_text[start_index: last_index]
     song_bot.send_message(message.chat.id, result_text)
 
 
 song_bot.infinity_polling()
-# replaced_singer = "Imagine-Dragons"
+# replaced_singer = "Imagine+Dragons"
 # replaced_song = "Believer"
 #
 # page = requests.get(f"https://www.google.com/search?q={replaced_singer}+{replaced_song}+lyrics")
